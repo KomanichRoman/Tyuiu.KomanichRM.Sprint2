8adc335afd50891a592fa1c0a9c023cd502fa602
Tyuiu.KomanichRM@outlook.com <gorotro_215@mail.ru>
2023-10-03 11:04:06 +0500

    Add project files.

---
+++ Tyuiu.KomanichRM.Sprint2.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KomanichRM.Sprint2.Task0.V27.Lib;
+
+namespace Tyuiu.KomanichRM.Sprint2.Task0.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 1305;
+            int y = 275;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { true, false, true, false, false, true };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KomanichRM.Sprint2.Task0.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KomanichRM.Sprint2.Task0.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KomanichRM.Sprint2.Task0.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b9ef6f4-194b-461a-812f-c4cb4e9bb911")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
